1b7de08bb60e08285c352469a26a4e21c542cbac
imattice <gogreen37@me.com>
2015-08-14 09:48:09 -0700

    add test for single letter match in single letter array and recieved failed test result

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,7 +1,7 @@
 <?php
     class RepeatCounter {
-        function countRepeats{
-            
+        function countRepeats(){
+
         }
     }
 ?>
---
+++ tests/countRepeatsTest.php
@@ -0,0 +1,41 @@
+<?php
+    require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase{
+
+        //User inputs a single letter and it is counted in an array with one matching single letter
+        // input-> "a" | "a"
+        // output-> 1
+        function test_countRepeats_single_letter_match()
+        {
+            $test_countRepeats = new RepeatCounter;
+            $input_letter = "a";
+            $input_list = "a";
+
+            $result = $test_RepeatCounter->countRepeats($input_letter, $input_list);
+
+            $this->assertEquals(1, $result);
+        }
+
+        //User inputs a single letter and it is not found in an array without any matching letters
+        // input-> "b" | "c"
+        // output-> "No matches found."
+
+        //User inputs a single letter and it is found, but does not count the unmatched letters
+        // input -> "d" | "d", "e", "f"
+        // output-> 1
+
+        //User inputs an array with multiple matches and each match is counted
+        //input-> "g" | "g", "g", "n", "g", "t"
+        //output-> 3
+
+        //User inputs a two-charactor string and it's match is counted in an array of strings
+        // input -> "hh" | "hh", "hi", "ij"
+        // output -> 1
+
+        //User can enter full words and parital matches will not be counted
+        // input-> "Good" | "Hello", "Goodbye", "Good Morning", "Good"
+        // output -> 1
+
+    }
+?>
