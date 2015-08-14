b96d4c6e41aeb3aec86c38e1d9af8b543e610aa3
imattice <gogreen37@me.com>
2015-08-14 10:59:56 -0700

    add webpage routing through Silex in app.php.

---
+++ app/app.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__."/../views"
+    ));
+
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('form.html.twig');
+    });
+
+    $app->get("results", function() use($app){
+        $input = $_GET['input'];
+        $repeats = new RepeatCounter;
+        return $app['twig']->render('results.html.twig', array('input' => input, 'result' => $repeats->countRepeats($input)));
+    });
+?>
