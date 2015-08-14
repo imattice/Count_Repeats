6ab59c4e4c8458374edb40b1c1e10f18c0ca5a8d
imattice <gogreen37@me.com>
2015-08-14 11:25:34 -0700

    enable debug in app.php.

--- app/app.php
+++ app/app.php
@@ -2,7 +2,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
 
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
     $app = new Silex\Application();
+    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__."/../views"
