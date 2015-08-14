8e10eac669934e124e26ed7e3f2bfc5c033cbbee
imattice <gogreen37@me.com>
2015-08-14 10:25:43 -0700

    add to countRepeats function to handle multiple items in array.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,13 +3,22 @@
         function countRepeats($input_word, $input_array){
 
             $count = 0;
-            if ($input_word == $input_array) {
-                $count++;
-                return $count;
-            } else {
-                return "No matches found.";
+            $exploded_array = explode(", ", $input_array);
+            $result = "";
+
+            //var_dump($exploded_array);
+            foreach ($exploded_array as $match){
+                if ($input_word == $match) {
+                    $count++;
+                };
             };
 
+            if ($count == 0){
+                $result = "No matches found.";
+            } else {
+                $result = $count;
+            }
+            return $result;
         }
     }
 ?>
